f6148fe47cfa83acbbee1870ee69305167cf0301
phanhuutoan <phanhuutoan.dev@outlook.com>
2023-03-12 14:35:06 +0700

    on Challenge 3

--- challenge 3/updateStack.sh
+++ challenge 3/updateStack.sh
@@ -7,4 +7,5 @@ PARAMS_PATH=$3
 aws cloudformation update-stack \
     --stack-name $NAME \
     --template-body file://$FILE_PATH \
-    --parameters file://$PARAMS_PATH 
+    --parameters file://$PARAMS_PATH \
+    --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM"
